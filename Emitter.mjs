790b913faa3e87112537284e06ccfd7753c080e8
yangyangyyluck <yangyangmd5@163.com>
2018-11-20 22:47:39 +0800

    add emitter注释

--- Emitter.mjs
+++ Emitter.mjs
@@ -1,8 +1,19 @@
+/**
+ * a simple implementation of Publish/Subscribe.
+ * @author yy <yangyangmd5@163.com>
+ * @module Emitter
+ */
 class Emitter {
+    /** @class */
     constructor() {
         this.types = new Map();
     }
 
+    /**
+     * subscribe a key, when the key had been publish, callback will be called.
+     * @param {string} type - subscribe key.
+     * @param {function} fn - callback.
+     */
     on(type, fn) {
         if (!type || typeof type !== 'string') {
             throw new TypeError('please make sure instance of type is string.');
@@ -16,6 +27,11 @@ class Emitter {
         }
     }
 
+    /**
+     * publish a key
+     * @param {string} type - key
+     * @param {*} data 
+     */
     emit(type, data) {
         if (!type || typeof type !== 'string') {
             throw new TypeError('please make sure instance of type is string.');
@@ -29,6 +45,7 @@ class Emitter {
         }
     }
 
+    /** clear all keys. */
     clear() {
         this.types.clear();
     }
